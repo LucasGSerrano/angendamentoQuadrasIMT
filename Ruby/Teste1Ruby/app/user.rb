0fd172cd79e553626986a5ab46a0a5174978406b
erica <erica_y_kido@hotmail.com>
2016-08-11 16:13:21 -0300

    new project in ruby

--- Ruby/Teste1Ruby/app/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-	attr_accessor :password
-
-	validates :username, :presence => true
-	validates :password, :confirmation => true
-end
--- Ruby/Teste1Ruby/routes.rb
+++
@@ -1,7 +0,0 @@
-root :to => "sessions#login"
-match "signup", :to => "users#new"
-match "login", :to => "sessions#login"
-match "logout", :to => "sessions#logout"
-match "home", :to => "sessions#home"
-match "profile", :to => "sessions#profile"
-match "setting", :to => "sessions#setting"
