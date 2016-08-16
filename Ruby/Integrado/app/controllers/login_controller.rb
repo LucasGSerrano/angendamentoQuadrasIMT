d4ea2fef0eb9f729373eaeb770e89ee473fa4b05
erica <erica_y_kido@hotmail.com>
2016-08-15 22:08:15 -0300

    change in login

--- Ruby/Integrado/app/controllers/login_controller.rb
+++ Ruby/Integrado/app/controllers/login_controller.rb
@@ -4,12 +4,14 @@ class LoginController < ApplicationController
 	end
 
 	def login
-		user = Usuario.where(:nome => params[:login][:nome].downcase)
-		if user && user.authenticate(params[:nome], params[:senha])
+		@user = Usuario.where(:nome => params[:login][:nome], :senha => params[:login][:senha] )
+
+		if @user.authenticate(params[:nome], params[:senha])
 			login[:nome] = user.nome
+			login[:senha] = user.senha
 			redirect_to usuarios_path, :notice => "Logado com Sucesso!"
 		else
-			redirect_to cadastro_usuarios_path
+			redirect_to new_cadastro_usuario_path
 		end
 	end
 end
--- Ruby/Integrado/app/models/usuario.rb
+++ Ruby/Integrado/app/models/usuario.rb
@@ -2,6 +2,6 @@ class Usuario < ActiveRecord::Base
 	validates_presence_of :nome, :senha
 
 	def self.authenticate(username, password)
-	    find(:first, :conditions=>["nome = ? AND senha = ?", username, password]) 
+	    find(:first, :conditions=>["nome = ? AND senha = ?", username, password] ) 
 	 end 
 end
