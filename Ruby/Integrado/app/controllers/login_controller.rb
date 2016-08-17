e180f780f61dc134dad9c8bd3d2dffe309fcbe7b
ericaykido <erica_y_kido@hotmail.com>
2016-08-17 09:18:50 -0300

    testing

--- Ruby/Integrado/app/controllers/login_controller.rb
+++ Ruby/Integrado/app/controllers/login_controller.rb
@@ -6,7 +6,7 @@ class LoginController < ApplicationController
 	def login
 		@user = Usuario.where(:nome => params[:login][:nome], :senha => params[:login][:senha]).first
 
-		if @user.authenticate(@user.nome, @user.senha )
+		if @user.nil? && @user.authenticate(@user.nome, @user.senha )
 			redirect_to usuarios_path, :notice => "Logado com Sucesso!"
 		end
 	end
