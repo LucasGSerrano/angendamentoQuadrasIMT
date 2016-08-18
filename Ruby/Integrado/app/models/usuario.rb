0b2959b26c0990042311860bd7d126c9fc3a125c
erica <erica_y_kido@hotmail.com>
2016-08-18 15:52:53 -0300

    change in create new user

--- Ruby/Integrado/app/models/usuario.rb
+++ Ruby/Integrado/app/models/usuario.rb
@@ -1,8 +1,8 @@
 class Usuario < ActiveRecord::Base
 
 	def authenticate(username, password)
-	    if self.nome == username && self.senha == password
-	      self
-	    end
+		if self.nome == username && self.senha == password
+	 		self
+		end
 	end
 end
