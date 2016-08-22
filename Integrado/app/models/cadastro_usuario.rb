5a74404543f5af0a75648abf78ac676d41542ba2
ericaykido <erica_y_kido@hotmail.com>
2016-08-22 12:20:02 -0300

    criptografia

--- Integrado/app/models/cadastro_usuario.rb
+++ Integrado/app/models/cadastro_usuario.rb
@@ -1,6 +1,15 @@
 class CadastroUsuario < ActiveRecord::Base
-	#has_secure_password
-
 	validates :senha, presence: true, length: { minimum: 6 }
 	validates :password_digest, presence: true, length: { minimum: 6 }
+
+  def senha=(new_password)
+    @senha = new_password
+    self.encrypted_password = password(@senha) if @senha.present?
+  end
+
+  private
+
+  def password(password)
+    ::BCrypt::Password.create(password)
+  end
 end
